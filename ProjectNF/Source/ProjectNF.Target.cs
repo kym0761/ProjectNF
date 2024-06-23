7c889d241dd54cb414fcfa0bcffb18c4a74a69d6
kym <kym0761@naver.com>
2024-06-23 19:38:53 +0900

    Add Basic Files

---
+++ ProjectNF/Source/ProjectNF.Target.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ProjectNFTarget : TargetRules
+{
+	public ProjectNFTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("ProjectNF");
+        ExtraModuleNames.Add("GamePuzzle");
+    }
+}
